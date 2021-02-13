17ca7ec68fb7ce095860c0677f9f97d50e2ccd15
Leon <leonlit123@gmail.com>
2021-02-13 10:00:27 +0800

    WIP

---
+++ module/initiate.py
@@ -0,0 +1,8 @@
+import module.utilities as utilities
+
+def initiateTracing (ipAddr):
+    userIP = utilities.getUserIpInfo()
+    return userIP # testing
+
+def domainNameValidation(domainName):
+    
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /bin/python3
 
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -8,12 +8,16 @@ def homePage():
     return render_template("index.html")
 
 # when user inputted the domain that he/she would like to trace
-import module.utilities as utilities
-@app.route("/tracedInfo/<string:jsonData>")
-def tracedInfo(jsonData):
-    # utilities.getUserIpLocation()
-    return render_template('tracedInfo.html', routesInfo = jsonData)
+import module.initiate as initiate
+@app.route("/tracedInfo/<string:ipAddr>")
+def tracedInfo(ipAddr):
+    routesInfo = initiate.initiateTracing(ipAddr)
+    return routesInfo
 
+@app.route("/checkDomain/<string:domainName>")
+def tracedInfo(domainName):
+    routesInfo = initiate.initiateTracing(ipAddr)
+    return routesInfo
 
 if __name__ == '__main__':
        app.run(debug = True)
