b700ddfcbf9e0a938684b0a052e417be039a45e7
Leon <leonlit123@gmail.com>
2021-02-28 20:58:45 +0800

    changed color list so that they're distinct from each other

--- assets/js/animatingTrace.js
+++ assets/js/animatingTrace.js
@@ -5,10 +5,11 @@ const mapLayer = new L.TileLayer('https://tiles.stadiamaps.com/tiles/alidade_smo
 // Adding layer to the map
 map.addLayer(mapLayer); */
 const colours = [
-    "#00ffff", "#f0ffff", "#f5f5dc", "#0000ff", "#a52a2a", "#00ffff", "#00008b", "#008b8b", "#a9a9a9",
-    "#006400","#bdb76b","#8b008b","#556b2f","#ff8c00","#9932cc","#8b0000","#e9967a","#9400d3","#ff00ff",
-    "#008000","#4b0082","#f0e68c","#add8e6","#e0ffff","#90ee90","#d3d3d3","#ffb6c1","#00ff00","#ff00ff",
-    "#800000","#000080","#808000","#ffa500","#ffc0cb","#800080","#800080","#ff0000","#c0c0c0"
+    '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231',
+    '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe',
+    '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000',
+    '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080',
+    '#ffffff', '#000000'
 ];
 
 let map;
